64065a9a962f207e8b9a662d877baaa793c6c3c5
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-03-16 13:06:24 +0500

    16-03-2023

---
+++ 16-03-2023 (2212C2)/app.js
@@ -0,0 +1,94 @@
+// let names=[]
+
+// // let howMany=+prompt("Enter How Many Values You Want To Enter?")
+
+// for(let input=0;input<4;input++){
+//     names[input]=prompt("Enter "+input+"th Value")
+   
+// }
+// console.log(names)
+
+// for(let j=0;j<names.length;j++){
+//     document.write("Your name is "+names[j]+"<br>")
+// }
+
+
+// let arr=["Dog","Cat","Elephant","Tiger"]
+
+// arr[0]="Dog"
+// arr[1]="Cat"
+
+// arr[2]="Lion"
+
+// arr.push("Lion")
+// arr.pop()
+
+// let arr=["Dog","Cat","Elephant","Tiger","Horse"]
+// arr.push("lion")  //["Dog","Cat","Elephant","Tiger","Horse","lion"]
+// arr.pop() //["Dog","Cat","Elephant","Tiger","Horse"]
+// arr.pop() //["Dog","Cat","Elephant","Tiger"]
+// arr.pop() //["Dog","Cat","Elephant"]
+// arr.push("Donkey") //["Dog","Cat","Elephant","Donkey"]
+// arr.pop() //["Dog","Cat","Elephant"]
+// arr.pop() //["Dog","Cat"]
+// arr.pop() //["Dog"]
+// arr.push("Girrafe") //["Dog","Girraffe"]
+// arr.push("Deer") //["Dog","giraffe","deer"]
+// arr.pop() //["Dog","Girraffe"]
+
+// console.log(arr)
+
+
+// let arr=["Dog","Cat","Elephant","Tiger","Horse"]
+
+// arr.push("Lion","Donkey","Deer")
+
+// console.log(arr)
+
+// let arr=["Dog","Cat","Elephant","Tiger","Horse"]
+
+// arr.shift()
+// arr.unshift("Lion","Deer")
+
+// arr.splice(2,3,"Lion")
+
+// arr.splice(2,2)
+
+
+// var newArr=arr.slice(4,5)
+
+// var newArr=arr.slice(1,3)
+
+
+// console.log(newArr)
+
+
+// let names=[]
+
+// let howMany=+prompt("Enter How Many Values You Want To Enter")
+
+// for(let i=0;i<howMany;i++){
+//     names.push(parseFloat(prompt("Enter "+i+"th Value")))
+// }
+
+// for(let j=0;j<names.length;j++){
+//     document.write("Your name is "+names[j]+"<br>")
+// }
+
+
+// let number=[1,2,3,4,5]  
+
+// let sum=0
+
+// for(let i=0;i<number.length;i++){
+//     sum=sum + number[i]
+// }
+
+
+// console.log(sum)
+
+// let number=[10,50,120,100,60]  
+// for(let i=0;i<number.length;i++){
+//     number[i]=number[i]+5
+//     console.log(number[i])
+// }
